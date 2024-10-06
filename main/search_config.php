39bc232faa19789b248378f5db237630d19ebc8b
junpataleta <junpataleta@users.noreply.github.com>
2024-10-06 03:08:09 +0000

    deploy: 929083a815c1f7e95c540173e7549810f6d4a598

--- main/search_config.php
+++ main/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "<script type=\"text/javascript\">\n/* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&amp;dn=expat.txt MIT */\n$(function(){ initResizable(false); });\n/* @license-end */\n</script>\n",
-  'logo' => "Generated on Sun Sep 29 2024 03:04:09 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.12.0 ",
+  'logo' => "Generated on Sun Oct 6 2024 03:04:00 for Moodle PHP Documentation by&#160;\n<a href=\"https://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.svg\" width=\"104\" height=\"31\" alt=\"doxygen\"/></a> 1.12.0 ",
 );
 
 ?>
